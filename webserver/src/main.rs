836890ab01b447b3717467ebf3664d6b8540998e
DaniloGregorio <dgg.trab@gmail.com>
2026-01-11 13:14:49 -0300

    created a server in actix-web(only examples rotes for testing)

---
+++ webserver/src/main.rs
@@ -0,0 +1,37 @@
+use actix_web::{HttpResponse, HttpServer, web,App, Responder,get,post};
+
+
+#[get("/one")]
+async  fn one() -> impl Responder{
+  HttpResponse::Ok().body("one")
+}
+
+#[post("/two")]
+async fn two()-> impl Responder{
+  HttpResponse::Ok().body("a")
+}
+//dynamic route
+#[get("/user/{id}")]
+async fn hello(path : web::Path<i32>) -> impl Responder{
+  let id = path.into_inner();
+  let msg = format!("{}",id);
+  HttpResponse::Ok().body(msg)
+}
+
+#[actix_web::main]
+async fn main(){
+  HttpServer::new(|| {
+    App::new()
+    .service(one)
+    .service(two)
+        .route("/", web::get().to(|| async {
+          HttpResponse::Ok().body("hello world".to_string())
+        }))
+    
+  }).bind("0.0.0.0:3000")
+    .unwrap()
+    .run()
+    .await 
+    .unwrap()
+
+}
